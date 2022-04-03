8e55d05866240d030518898aad29d33cf123b0fc
Daniel Hellstern <git@daniel.hellstern.org>
2022-04-03 17:20:02 -0400

    Added reverse DNS for nameservers

--- CCC-412/Sprint 3/zones/in-addr.arpa.js
+++ CCC-412/Sprint 3/zones/in-addr.arpa.js
@@ -1,4 +1,7 @@
 D(REV("172.23.119.64/27") + "!reverse", REG_NONE, DnsProvider(DNS_BIND),
     NAMESERVER("ns-1.champlain.dn42."),
-    NAMESERVER("ns-2.champlain.dn42.")
+    NAMESERVER("ns-2.champlain.dn42."),
+
+    PTR("172.23.119.66", "ns-1.champlain.dn42."),
+    PTR("172.23.119.67", "ns-2.champlain.dn42.")
 )
